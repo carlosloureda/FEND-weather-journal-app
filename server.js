85881f51188ea4a3f1bf1f1a4e04878240ea0619
Carlos Loureda <balanzeneto@gmail.com>
2020-02-06 03:33:22 +0100

    [feat] Added express, body-parser & cors dependencies
    Made some changes in server.js to require these 3 libraries to see that everything is
    working properly

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-// The Express app instance should be pointed to the project folder with .html, .css, and .js files.
-
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
 // Require Express to run server and routes
+const express = require("express");
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+const bodyParser = require("body-parser");
 //Here we are configuring express to use body-parser as middle-ware.
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
-// app.use(express.static("website"));
+app.use(express.static("website"));
 
 // Setup Server
 
